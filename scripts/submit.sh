7b9edcf6c90e93b362ffca79951661ad8e4235a5
Karim Alibhai <karim@alibhai.co>
2020-03-27 02:46:47 -0400

    Fix: submission script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -5,10 +5,15 @@ node "$(dirname $0)/generate-sql.js"
 
 # generate full submission
 tmp="$(mktemp -d)"
-rsync -a "$(dirname $0)/.." "${tmp}/dashboard" --exclude node_modules --exclude .git --exclude .pgdata
-mv "${tmp}/dashboard/submission/images" "${tmp}/dashboard/submission/queries.sql" "${tmp}/"
-rm -rf ${tmp}/dashboard/{dist,.cache,.pgdata,node_modules,submission,.git}
-zip -r "submission.zip" ${tmp}
+rsync -a "$(dirname $0)/.." $tmp/dashboard --exclude node_modules --exclude .git --exclude .pgdata
+mv $tmp/dashboard/submission/* "${tmp}/"
+rm -rf $tmp/dashboard/{dist,.cache,.pgdata,node_modules,submission,.git}
+
+# create archive
+WD="$PWD"
+cd "$tmp"
+rm -f "$WD/submission.zip"
+zip -r "$WD/submission.zip" *
 
 # cleanup
 rm -rf "$tmp"
